fde82bdad720e90816f1ee6676f875cd6c1ae765
Your Name <linnea.rustan@gmail.com>
2026-05-07 20:02:04 +0200

    Added main classifiers and code

---
+++ README.md
@@ -0,0 +1,2 @@
+## READ ME
+Run the jupyter file "Task1-3_2_labels.ipynb" and then the python file "Task4_neural_network.py" or "Task4_random_forest.py" depending on which model you want to connect to the live brooker. 
---
+++ Task4_random_forest.py
@@ -0,0 +1,91 @@
+import paho.mqtt.client as mqtt
+import uuid #Unique user id
+import json
+
+N = 5
+
+BROKER = "emaintlab-D0042E-broker.azurewebsites.net"
+TOPICS = [
+    "emaint/house1/sensor/temperature/room1",
+    "emaint/house1/sensor/temperature/room2",
+    "emaint/house1/sensor/temperature/room3",
+    "emaint/house1/sensor/temperature/supply",
+    "emaint/house1/sensor/temperature/outside",
+    "emaint/house1/sensor/flow/room1",
+    "emaint/house1/sensor/flow/room2",
+    "emaint/house1/sensor/flow/room3",
+    "emaint/house1/sensor/return/room1",
+    "emaint/house1/sensor/return/room2",
+    "emaint/house1/sensor/return/room3",
+]
+
+# Callback when connected
+def on_connect(client, userdata, flags, rc, properties):
+    print("Connected with result code", rc)
+    for topic in TOPICS:
+        client.subscribe(topic)
+
+buffer = [[],[],[],[],[],[],[],[],[],[],[]]
+
+def on_message(client, userdata, msg):
+    value = json.loads(msg.payload)['value']
+    topic = msg.topic
+    topic_index = TOPICS.index(topic)
+    #print(topic_index,topic,value)
+    if len(buffer[topic_index]) < N:
+        buffer[topic_index].append(value)
+    else:
+        buffer[topic_index] = buffer[topic_index][1:]+[value]
+
+
+client_id = f"client-{uuid.uuid4()}"
+client = mqtt.Client(
+    client_id = client_id,
+    transport="websockets",
+    callback_api_version=mqtt.CallbackAPIVersion.VERSION2
+)
+username = "username" # set username
+password = "password" # set password
+client.username_pw_set(username,password)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.tls_set()
+
+client.connect(BROKER, 443, 60)
+
+    
+import pickle
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+with open('tree_model.pkl', 'rb') as f:
+    clf = pickle.load(f)
+
+import numpy as np
+def main():
+    X_all = np.array(buffer).T
+    X_raw = X_all[-1:,:]
+    X_sp_err = np.array([[21,22,20]]) - X_raw[:,0:3]
+    X_P = X_raw[:,5:8]*(X_raw[:,3:4] - X_raw[:,-3:])
+    X_mean = np.mean(X_all,axis=0,keepdims=True)
+    #X_std = np.std(X_all,axis=0,keepdims=True)
+
+    X = np.concatenate([X_raw, X_sp_err, X_P, X_mean], axis = 1)
+    
+    prediction = clf.predict_proba(X)
+    print(f"Probability of no fault: {prediction[0,0]*100:.2f} %")
+    print(f"Probability of fault: {prediction[0,1]*100:.2f} %")
+    print()
+
+import time
+try:
+    client.loop_start()
+    time.sleep(5)
+    while True:
+        main()
+        time.sleep(15)
+finally:
+    client.loop_stop()
+    client.disconnect()
+
